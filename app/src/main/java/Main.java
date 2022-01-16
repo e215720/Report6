a2164a18bd71e2dd7c221a8071df1705a6e59ac0
kazuki kinjo <e215720@ie.u-ryukyu.ac.jp>
2022-01-16 11:57:20 +0900

    5回目

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,11 +1,25 @@
 import jp.ac.uryukyu.ie.e215720.*;
+import jp.ac.uryukyu.ie.e215720.Character;
 
 public class Main {
     public static void main(String[] args){
-        GameMaster master = new GameMaster("マスター");
-        for(var i=0; i<5; i++){
-            master.showStatus();
-            master.battle();
-        }
+        //GameMaster master = new GameMaster("マスター");
+        //for(var i=0; i<4; i++){
+            //master.showStatus();
+            //master.battle();
+        //}
+        int hp = 10;
+        Player player1 = new Player("デモ１", hp);
+        Player player2 = new Player("デモ2", hp);
+        Character cha1 = new Character("キャラ1", hp, 10);
+        Character cha2= new Character("キャラ2",2,5);
+        Character cha3= new Character("キャラ3",2,5);
+        player1.addCharacter(cha1);
+        player2.addCharacter(cha2);
+        player2.addCharacter(cha3);
+        cha1.act(player2);
+        cha2.act(player1);
+        cha3.act(player1);
+
     }
 }
