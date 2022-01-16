bff4caa946d9ee3a3cf537370b67c261c4c4e153
kazuki kinjo <e215720@ie.u-ryukyu.ac.jp>
2022-01-16 11:39:36 +0900

    3回目

--- app/src/test/java/jp/ac/uryukyu/ie/e215720/PlayerTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e215720/PlayerTest.java
@@ -7,11 +7,19 @@ public class PlayerTest {
         int hp = 10;
         Player player1 = new Player("デモ１", hp);
         Player player2 = new Player("デモ2", hp);
-        player1.addCharacter(new Character("キャラ1",2,10));
-        player2.addCharacter(new Character("キャラ2",2,5));
-        player2.addCharacter(new Character("キャラ3",2,5));
-        assertEquals(1,player1.getCharacters().size());
-        assertEquals(2,player2.getCharacters().size());
+        Character cha1= new Character("キャラ1",2,10);
+        Character cha2= new Character("キャラ2",2,5);
+        Character cha3= new Character("キャラ3",2,5);
+        player1.addCharacter(cha1);
+        player2.addCharacter(cha2);
+        player2.addCharacter(cha3);
+        cha1.act(player2);
+        cha2.act(player1);
+        cha3.act(player1);
+        assertEquals(0,player2.getHp());
+        assertEquals(0, player1.getHp());
+        assertEquals(1, player1.getCharacters().size());
+        assertEquals(2, player2.getCharacters().size());
     }
 
     
